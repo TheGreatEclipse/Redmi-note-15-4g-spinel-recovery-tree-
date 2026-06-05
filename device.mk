f98a73d9812368d204e293f3a6b16e3625ff60f8
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-05 21:39:05 +0300

    fix: use MTK boot HAL impl, add keymaster/plpath for FBE decryption

--- device.mk
+++ device.mk
@@ -20,11 +20,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_vendor=erofs \
     POSTINSTALL_OPTIONAL_vendor=true
 
-# Boot control HAL (1.2 for Android 13+/16)
+# Boot control HAL (MTK implementation for Android 13+/16)
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6789 \
@@ -50,15 +49,40 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
-# Snapuserd (required for erofs decryption/OTA)
+# Keymaster / KeyMint (needed for FBE decryption)
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1 \
+    android.hardware.security.keymint \
+    android.hardware.security.secureclock \
+    android.hardware.security.sharedsecret
+
+# MTK plpath utils (symlinks for preloader partitions)
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
+
+# Snapuserd (required for erofs / Virtual A/B OTA)
 PRODUCT_PACKAGES += \
     snapuserd
 
+# Additional crypto libs
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
 
 # Shipping API level (Android 16 = API 36)
 PRODUCT_SHIPPING_API_LEVEL := 35
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/spinel
+
+# Property: vendor_boot recovery flag
+PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
