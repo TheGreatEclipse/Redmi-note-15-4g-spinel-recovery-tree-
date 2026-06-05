9399080b03480bf4e880acedd347bd18f1833cc4
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-05 20:39:03 +0300

    fix(device.mk): boot HAL 1.2, fastbootd, snapuserd, checkpoint_gc

--- device/xiaomi/spinel/device.mk
+++ device/xiaomi/spinel/device.mk
@@ -1,27 +1,32 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2026 TWRP Device Tree – Redmi Note 15 4G (spinel)
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := device/xiaomi/spinel
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+# Boot control HAL (1.2 for Android 13+/16)
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6789
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6789 \
     libgptutils \
     libz \
@@ -30,6 +35,30 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
+    checkpoint_gc \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Snapuserd (required for erofs decryption/OTA)
+PRODUCT_PACKAGES += \
+    snapuserd
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Shipping API level (Android 16 = API 36)
+PRODUCT_SHIPPING_API_LEVEL := 35
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += device/xiaomi/spinel
