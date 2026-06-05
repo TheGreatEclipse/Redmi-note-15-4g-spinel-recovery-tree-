3262776dc16d55fc602db26a6b79a639300be16f
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-05 21:38:58 +0300

    fix: replace add_lunch_combo with COMMON_LUNCH_CHOICES [build fix]

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_spinel.mk
 
+# Use COMMON_LUNCH_CHOICES instead of deprecated add_lunch_combo (vendorsetup.sh)
 COMMON_LUNCH_CHOICES := \
     twrp_spinel-user \
     twrp_spinel-userdebug \
