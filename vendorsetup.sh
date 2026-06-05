750757aeb57096358d1917c14797c69a4b77e921
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-05 21:39:00 +0300

    fix: remove deprecated add_lunch_combo calls from vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,9 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2026 TWRP Device Tree – Redmi Note 15 4G (spinel)
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-add_lunch_combo twrp_spinel-user
-add_lunch_combo twrp_spinel-userdebug
-add_lunch_combo twrp_spinel-eng
+# NOTE: add_lunch_combo is OBSOLETE since Android 10+.
+# Lunch choices are now declared via COMMON_LUNCH_CHOICES in AndroidProducts.mk
+# This file is kept empty intentionally to avoid build warnings.
