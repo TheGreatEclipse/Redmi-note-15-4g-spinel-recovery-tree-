11abb9dcfe6b8c8c26f689e85669e432bc9a98f1
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-05 21:51:09 +0300

    chore(AndroidProducts.mk): update for OrangeFox R12 + TWRP Latest vendor_boot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,15 +1,19 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 TWRP Device Tree – Redmi Note 15 4G (spinel)
+# Copyright (C) 2026 TWRP/OrangeFox R12 Device Tree – Redmi Note 15 4G (spinel)
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_spinel.mk
+    $(LOCAL_DIR)/twrp_spinel.mk \
+    $(LOCAL_DIR)/fox_spinel.mk
 
 # Use COMMON_LUNCH_CHOICES instead of deprecated add_lunch_combo (vendorsetup.sh)
 COMMON_LUNCH_CHOICES := \
     twrp_spinel-user \
     twrp_spinel-userdebug \
-    twrp_spinel-eng
+    twrp_spinel-eng \
+    fox_spinel-user \
+    fox_spinel-userdebug \
+    fox_spinel-eng
