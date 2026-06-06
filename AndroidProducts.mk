dd82914205abebe3c9f2d821f340445eb1563cbc
mkpromvp <tabmohamedpro@gmail.com>
2026-06-06 20:29:20 +0300

    spinel: fix AndroidProducts and sync build workflow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,17 +1,3 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 TWRP/OrangeFox R12 Device Tree - Redmi Note 15 4G (spinel)
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+PRODUCT_MAKEFILES :=     $(LOCAL_DIR)/fox_spinel.mk
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_spinel.mk \
-    $(LOCAL_DIR)/fox_spinel.mk
-
-# COMMON_LUNCH_CHOICES registers products in lunch (replaces deprecated vendorsetup.sh)
-COMMON_LUNCH_CHOICES := \
-    twrp_spinel-eng \
-    twrp_spinel-userdebug \
-    fox_spinel-eng \
-    fox_spinel-userdebug
+COMMON_LUNCH_CHOICES :=     fox_spinel-eng     fox_spinel-userdebug
